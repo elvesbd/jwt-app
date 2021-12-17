36dad761bb557dbb1a94d46126af5cb8faad802b
elvesbd <elvesbd@yahoo.com>
2021-12-17 17:47:04 -0300

    apply config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,21 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { join } from 'path';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'Vaibem@123',
-      database: 'jwtapp',
+      type: process.env.TYPEORM_CONNECTION,
+      host: process.env.TYPEORM_HOST,
+      port: process.env.TYPEORM_PORT,
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
       entities: join[__dirname + '/**/*.entity{.js,.ts}'],
-    }),
+      synchronize: true,
+    } as TypeOrmModuleOptions),
   ],
   controllers: [],
   providers: [],

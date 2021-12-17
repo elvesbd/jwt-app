dd0329576b34742ff491c76e03867f0cc9ee9ccd
elvesbd <elvesbd@yahoo.com>
2021-12-17 19:46:56 -0300

    added create user dto eand update dto

--- src/app/users/repositories/users-repositories.ts
+++ src/app/users/repositories/users-repositories.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { FindConditions, FindOneOptions } from 'typeorm';
+import { UserEntity } from '../infra/entity/user-entity';
 import { UsersRepository } from '../infra/typeorm/users-repository';
 
 @Injectable()
@@ -8,4 +10,32 @@ export class UsersRepositories {
     @InjectRepository(UsersRepository)
     private readonly usersRepository: UsersRepository,
   ) {}
+
+  async findAll() {
+    return await this.usersRepository.find({
+      select: ['id', 'firstName', 'lastName', 'email'],
+    });
+  }
+
+  async findOneOrFail(
+    conditions: FindConditions<UserEntity>,
+    options?: FindOneOptions<UserEntity>,
+  ) {
+    try {
+      return await this.usersRepository.findOneOrFail(conditions, options);
+    } catch (e) {
+      throw new NotFoundException(e.message);
+    }
+  }
+
+  async update(id: string, data) {
+    const user = await this.findOneOrFail({ id });
+    this.usersRepository.merge(user, data);
+    return await this.usersRepository.save(user);
+  }
+
+  async destroy(id: string) {
+    await this.usersRepository.findOneOrFail({ id });
+    this.usersRepository.softDelete({ id });
+  }
 }
--- src/app/users/services/users.service.ts
+++ src/app/users/services/users.service.ts
@@ -4,4 +4,20 @@ import { UsersRepositories } from '../repositories/users-repositories';
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepositories: UsersRepositories) {}
+
+  async findAll() {
+    return await this.usersRepositories.findAll();
+  }
+
+  async findOneOrFail(conditions, options) {
+    return await this.usersRepositories.findOneOrFail(conditions, options);
+  }
+
+  async update(id: string, data) {
+    return await this.usersRepositories.update(id, data);
+  }
+
+  async destroy(id: string) {
+    return await this.usersRepositories.destroy(id);
+  }
 }

adfb26c84ef11c66c35e7a25f5b2f7c9c7da1277
elvesbd <elvesbd@yahoo.com>
2021-12-18 19:48:59 -0300

    created class local strategy

--- src/helpers/messages.helpers.ts
+++ src/helpers/messages.helpers.ts
@@ -2,4 +2,5 @@ export const MessagesHelper = {
   PASSWORD_VALID:
     'A senha deve conter letras maiúsculas, minúsculas, números e caracteres especiais',
   EMAIL_VALID: 'precisa ser um e-mail válido',
+  PASSWORD_OR_EMAIL_INVALID: 'E-mail e/ou senha informada estão inválidos',
 };
